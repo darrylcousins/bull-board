06de9cb96b7edd4de748e1a668bd6cb6e95f73d8
cousinsd <darryljcousins@gmail.com>
2023-06-21 16:02:41 +1200

    Added ejs engine and simple authentication (.env required)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import crypto from "crypto";
 import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 import { createBullBoard } from "@bull-board/api";
 import { BullMQAdapter } from "@bull-board/api/bullMQAdapter.js";
@@ -7,6 +10,16 @@ import { ExpressAdapter } from "@bull-board/express";
 import { rechargeQueue } from "./queue.js";
 
 const port = process.env.NODE_PORT || 3000;
+
+const getHashedPassword = (password) => {
+  const sha256 = crypto.createHash('sha256');
+  const hash = sha256.update(password).digest('base64');
+  return hash;
+};
+
+const generateAuthToken = () => {
+  return crypto.randomBytes(30).toString('hex');
+};
 
 /* Bullboard */
 const serverAdapter = new ExpressAdapter();
@@ -22,12 +35,75 @@ serverAdapter.setBasePath("/admin");
 /* Express */
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+/* Ensure authentication */
+app.use((req, res, next) => {
+  // Get auth token from the cookies
+  const authToken = req.cookies['AuthToken'];
+
+  // Inject the user to the request
+  req.user = authTokens[authToken];
+
+  next();
+});
+
+/* Middleware */
+const requireAuth = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.render('login', {
+      message: 'Please login to continue',
+      messageClass: 'alert-danger'
+    });
+  }
+};
+
+  
 /* Routes */
+//app.use("/admin", serverAdapter.getRouter());
+app.use('/admin', requireAuth, serverAdapter.getRouter());
 
-app.use("/admin", serverAdapter.getRouter());
+app.get("/", requireAuth, async function (req, res) {
+  res.render("index");
+});
 
-app.get("/", async function (req, res) {
-  res.render("index", { port });
+app.get("/login", async function (req, res) {
+  res.render("login", { message: null });
+});
+
+const authTokens = {};
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = getHashedPassword(password);
+
+  const user = process.env.USER === username && process.env.PASS === hashedPassword;
+
+  if (user) {
+    const authToken = generateAuthToken();
+
+    // Store authentication token
+    authTokens[authToken] = username;
+
+    // Setting the auth token in cookies
+    res.cookie('AuthToken', authToken);
+
+    // Redirect user to the protected page
+    res.redirect('/admin');
+  } else {
+    res.render('login', {
+      message: 'Invalid username or password',
+      messageClass: 'alert-danger'
+    });
+  };
 });
 
 /* End Routes */
